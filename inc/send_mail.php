ea57b1efbe6a044a9bd72dcded482e0b5c8c44ba
Pierre-Aurélien GEORGES <pageorge@unice.fr>
2018-09-07 14:50:15 +0200

    FIX: envoi de mails fonctionne à nouveau UPDATE: .DS_Store ajouté au .gitignore
    P.A.

---
+++ inc/send_mail.php
@@ -0,0 +1,28 @@
+<?php 
+
+// fonction trouvée sur http://bitprison.net/content/how-to-send-correct-utf-8-mail-in-php/
+function mail_utf8($to, $subject = '(No subject)', $message = '', $header = '') {
+  $header_ = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/plain; charset=UTF-8' . "\r\n";
+  mail($to, '=?UTF-8?B?'.base64_encode($subject).'?=', $message, $header_ . $header);
+}
+
+function send_mail_to_user($id, $ticket, $what_changed = "Votre ticket à été mis à jour. Vous pouvez le consulter à l'adresse") {
+	$to = $ticket->email;
+	$subject = "[BCL Ticket n°". $id ."] " . $ticket->objet;
+	$txt = $what_changed . " : http://bcl.unice.fr/bcl-helpdesk/tickets.html?id=" . $id;
+	$txt = $txt . "\r\n\r\n" . "Description : " . "\r\n\r\n" . $ticket->description;
+	$headers = "From: bcl-service-info@unice.fr";
+	mail_utf8($to,$subject,$txt,$headers);
+}
+
+function send_mail_to_admins($id, $ticket, $what_changed = 'Mise à jour du ticket') {
+	$to = "bcl-service-info@unice.fr";
+	//$to = "laurent.vanni@unice.fr";
+	$subject = "[BCL Ticket n°". $id ."] " . $ticket->objet;
+	$txt = $what_changed . " : http://bcl.unice.fr/bcl-helpdesk/admin/?id=" . $id;
+	$txt = $txt . "\r\n\r\n" . "Description : " . "\r\n\r\n" . $ticket->description;
+	$headers = "From: bcl-service-info@unice.fr";
+	mail_utf8($to,$subject,$txt,$headers);
+}
+
+?>
