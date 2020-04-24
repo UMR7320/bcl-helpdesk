f7f02ace584bc94ed67f5378307dde20dd9080cf
Laurent Vanni <lvanni@unice.fr>
2020-04-24 12:10:11 +0200

    FIX: get client IP

--- tickets.php
+++ tickets.php
@@ -1,3 +1,24 @@
+<?php 
+
+function getRealIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+?>
+
 <!DOCTYPE HTML>
 <!--
 	Spectral by HTML5 UP
@@ -42,7 +63,7 @@
 						</nav>
 					</header>
 
-				<?php if (strpos("134.59.", $_SERVER['REMOTE_ADDR']) === false) { ?>
+				<?php if (strpos("134.59.", getRealIpAddr()) === false) { ?>
 
 					<!-- RESTRICTED AREA MODAL -->
 					<div id='restricted_modal' class='modal'>
